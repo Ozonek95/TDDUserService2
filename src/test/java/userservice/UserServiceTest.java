88aa2cbd4b5f7c5141c7e834f85d5cd1bfc2f649
Kacper <kacperstaszek1995@gmail.com>
2018-10-24 18:19:35 +0200

    create test of Validator

--- src/test/java/userservice/UserServiceTest.java
+++ src/test/java/userservice/UserServiceTest.java
@@ -3,6 +3,8 @@ package userservice;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 
 public class UserServiceTest {
@@ -90,5 +92,12 @@ public class UserServiceTest {
         }
 
 
+    }
+
+    @Test
+    public void checkIfValidatorMethodWorks() {
+        LoginValidator loginValidator = Mockito.mock(LoginValidator.class);
+
+        Assert.assertTrue(loginValidator.validate("CorrectLogin"));
     }
 }
